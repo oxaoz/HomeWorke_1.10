56c054fee331ee82cd15940ab26b7aa543efaeeb
oxaoz <ogozer@ya.ru>
2022-10-26 12:45:08 +0700

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Строки. Задача 1.
+        System.out.println("Строки. Задача 1.");
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("ФИО сотрудника - " + fullName);
     }
 }
+
+
